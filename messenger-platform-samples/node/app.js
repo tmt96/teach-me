76508a3af3bf08cb088dac17a07bfe9b2a9e7b1b
hychanh <hychanh123@gmail.com>
2016-07-30 20:40:01 +0700

    Update app.js

--- messenger-platform-samples/node/app.js
+++ messenger-platform-samples/node/app.js
@@ -386,7 +386,7 @@ function receivedAccountLink(event) {
  * Send an image using the Send API.
  *
  */
-function sendImageMessage(recipientId) {
+function sendImageMessage(recipientId,linking) {
   var messageData = {
     recipient: {
       id: recipientId
@@ -395,7 +395,7 @@ function sendImageMessage(recipientId) {
       attachment: {
         type: "image",
         payload: {
-          url: SERVER_URL + "/assets/rift.png"
+          url: lingking
         }
       }
     }
@@ -854,7 +854,12 @@ function translateAndSend(recipientId, original) {
     var translated = data.translated;
     var user = UsersRepository.get(recipientId);
         user.reqIncr();
+    var image="";
+    image=data.image;
     sendTextMessage(recipientId, translated);
+    if (image!="") sendImageMessage(recipientId, image)
+    
+    
     if( user.meetLevelUp() ){
         sendGifMessage(recipientId);
     }
