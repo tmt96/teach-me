7ab61491af61c96ac2ec09f82e895e49f5e9e9a3
Tran Phong Phu <vietean@gmail.com>
2016-07-31 12:35:49 +0700

    Fix bug

--- libs/user.js
+++ libs/user.js
@@ -32,5 +32,49 @@ User.prototype.meetLevelUp = function()
     return false;
 };
 
+User.prototype.setQuestions = function(questions)
+{
+    this.questions = questions;
+    this.createAnswersList();
+};
+
+User.prototype.createAnswersList = function()
+{
+    var arrAnswers = [];
+    for (var i=0; i < this.questions.length; i++) {
+        console.log(i, this.questions[i].translated);
+        arrAnswers[i] = this.questions[i].translated;
+    }
+    this.arrAnswers = arrAnswers;
+};
+
+User.prototype.getAnswerList = function() {
+    return this.arrAnswers;
+}
+
+User.prototype.getAQuestionAndRemoveOutOfStack = function()
+{
+    if( !this.questions.length ){
+        return null;
+    }
+    var question = this.questions.shift();
+    this.originalWord = question.word;
+    this.correctAnswer = question.translated;
+    return question;
+};
+
+User.prototype.turnOffReview = function()
+{
+  this.reviewOn = false;
+  this.arrAnswers = [];
+  this.questions = [];
+  this.originalWord = '';
+  this.correctAnswer = '';
+}
+
+User.prototype.turnOnReview = function(){
+    this.reviewOn = true;
+}
+
 
 module.exports = User;
--- libs/users.repository.js
+++ libs/users.repository.js
@@ -11,48 +11,4 @@ var UsersRepository = {
     }
 };
 
-User.prototype.setQuestions = function(questions)
-{
-    this.questions = questions;
-    this.createAnswersList();
-};
-
-User.protoptype.createAnswersList = function()
-{
-    var arrAnswers = [];
-    for (var i=0; i < this.questions.length; i++) {
-        console.log(i, this.questions[i].translated);
-        arrAnswers[i] = this.questions[i].translated;
-    }
-    this.arrAnswers = arrAnswers;
-};
-
-User.prototype.getAnswerList = function() {
-    return this.arrAnswers;
-}
-
-User.prototype.getAQuestionAndRemoveOutOfStack = function()
-{
-    if( !this.questions.length ){
-        return null;
-    }
-    var question = this.questions.shift();
-    this.originalWord = question.word;
-    this.correctAnswer = question.translated;
-    return question;
-};
-
-User.prototype.turnOffReview = function()
-{
-  this.reviewOn = false;
-  this.arrAnswers = [];
-  this.questions = [];
-  this.originalWord = '';
-  this.correctAnswer = '';
-}
-
-User.prototype.turnOnReview = function(){
-    this.reviewOn = true;
-}
-
 module.exports = UsersRepository;
